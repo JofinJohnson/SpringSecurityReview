c166359e9184e0924f0f6101bb33895eba4a912e
JofinJohnson <jofinjohnson.p@gmail.com>
2023-08-16 23:57:12 +0530

    Default Spring Security

---
+++ SpringSecurityReviewOne/src/test/java/com/review/spring/SpringSecurityReviewOne/SpringSecurityReviewOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.spring.SpringSecurityReviewOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReviewOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
