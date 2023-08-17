2b12739e102bca5dc1c78de3a412b6ad2e23d7ae
JofinJohnson <jofinjohnson.p@gmail.com>
2023-08-17 17:08:15 +0530

    App 2 - auth with H2 database with default schema

---
+++ spring-security-two/src/test/java/com/review/spring/springsecuritytwo/SpringSecurityTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.spring.springsecuritytwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
